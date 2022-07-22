a24d1f93cf1ec00be7506ae8b6943aa1a861c96e
AndrewK0704 <andrew.kozhenov@mail.ru>
2022-07-22 20:24:02 +0300

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+
+  let str1=str;
+  
+    for(let i=0; i<str1.length; i++){
+      for (let j=0; j<bracketsConfig.length; j++){
+        if (str1.length!==2 && str1[i]===bracketsConfig[j][0] && str1[i+1]===bracketsConfig[j][1]){        
+          str1=str1.slice(0,i)+str1.slice(i+1);
+          str1=str1.slice(0,i)+str1.slice(i+1);
+          i=0;
+          j=bracketsConfig.length;          
+        }     
+      }    
+    }
+
+    for (j=0; j<bracketsConfig.length; j++){
+      if (str1.length===2 && str1[0]===bracketsConfig[j][0] && str1[1]===bracketsConfig[j][1]){
+        str1='';        
+      }      
+    } 
+  
+  return str1.length===0; 
+
 }
